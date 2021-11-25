2c9311745d336e5b56e8a45578685c23ca11da34
PahwaKartik22 <pahwakartik22@gmail.com>
2021-11-26 00:28:47 +0530

    Add files via upload

---
+++ PrintingAllPrimeNumberTillN.java
@@ -0,0 +1,24 @@
+package lec2cont;
+
+import java.util.Scanner;
+
+public class PrintingAllPrimeNumberTillN {
+	public static void main(String args[])
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int  i = 2;
+	    int n= sc.nextInt();
+	    for(i=2;i<=n;i++) 
+	    {
+	    for(int d =2;d<i;d=d+1)
+	        {
+	        if(i%d==0) 
+	        i++;
+	        continue;
+	        }
+	        
+	    }
+	        System.out.println(i);
+	        
+	    }
+	}
