243a2b50b8b7c20f23934791d11d282a04e98d0c
PahwaKartik22 <pahwakartik22@gmail.com>
2021-11-26 19:02:05 +0530

    Add files via upload

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,26 +1,22 @@
-import java.util.Scanner;
-public class DecimalToBinary
-{
-    
-    public static void main(String[] args) 
-{
-        Scanner sc =new Scanner(System.in);
-        int n = sc.nextInt();
+package asnmentConditionLoop;
 
-        for(int i =17;i>=0;i--)
-        {
-            int r = n>>i;
-            
-            if((r&1)>0)
-            {
-                System.out.print("1");
-            
-            }
-            else 
-            {
-                System.out.print("0");
-            }
-        }
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String args[])
+    { Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      long x = 0,i = 1; 
+      if ((0<=n)&&(n<=100000))
+      while (n!=0)
+      { int t = n%2;
+        x = x + t*i;
+        n/=2;
+        i= i*10; 
         
-}
-}
+      }
+      System.out.print(x);
+      
+    }
+        
+    }
